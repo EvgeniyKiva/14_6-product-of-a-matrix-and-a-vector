9d8c6622246f189fe984d81d0fa19398d912e55c
EvgeniyKiva <97800267+EvgeniyKiva@users.noreply.github.com>
2022-02-21 19:35:53 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main() {
 
     for (int i=0; i<4; i++){
         for (int j=0; j<4; j++){
-            r[i] += v[i]*m[i][j];
+            r[i] += v[j]*m[i][j];
             //std::cout << r[i] << " ";
 
         }
